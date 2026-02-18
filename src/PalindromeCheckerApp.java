6fd2c68562392f1271aba449401ab5154ae2e3a7
Bhoomithakur26 <thakurbhoomi2626@gmail.com>
2026-02-18 10:47:13 +0530

    UC3-StringReverse

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -3,6 +3,19 @@ public class PalindromeCheckerApp {
 System.out.println ("Welcome to the Palindrome Checker management system");
         System.out.println ("Version : 1.0");
         System.out.println ("System initialized Successfully");
+        for(int i = input.length()-1; i>=0; i--){
+            reverse += input.charAt(i);
+        }
+
+        if(input.equals(reverse)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
+        sc.close();
+
+        }
     }
 
-}
+
